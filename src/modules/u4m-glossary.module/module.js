249e81dd012d9b45fb4b37abee8ffca50ddf561c
pinkiwebplant <pinki@thewebplant.in>
2025-02-24 19:06:55 +0530

    glossary css updates

--- src/modules/u4m-glossary.module/module.js
+++ src/modules/u4m-glossary.module/module.js
@@ -1,35 +1,39 @@
 $(document).ready(function () {
-	function submitForm() {
-		let formData = $(".glossTop").serialize(); // Get form data
+  function submitForm() {
+    let formData = $(".glossTop").serialize(); // Get form data
 
-		$.ajax({
-			url: window.location.pathname, // Keep current URL
-			type: "GET",
-			data: formData,
-			beforeSend: function () {
-				$(".glossList").html("<p>Loading...</p>");
-			},
-			success: function (response) {
-				let newHtml = $(response).find(".glossList").html();
-				$(".glossList").html(newHtml);
-			},
-			error: function () {
-				$(".glossList").html("<p>Error loading results.</p>");
-			},
-		});
-	}
+    $.ajax({
+      url: window.location.pathname, // Keep current URL
+      type: "GET",
+      data: formData,
+      beforeSend: function () {
+        $(".glossList").html("<p>Loading...</p>");
+      },
+      success: function (response) {
+        let newHtml = $(response).find(".glossList").html();
+        $(".glossList").html(newHtml);
+      },
+      error: function () {
+        $(".glossList").html("<p>Error loading results.</p>");
+      },
+    });
+  }
 
-	// Auto-submit on search input change
-	$(".glossSearch input[type='search']").on("input", function () {
-		submitForm();
-	});
+  // Auto-submit on search input change
+  $(".glossSearch input[type='search']").on("input", function () {
+    submitForm();
+  });
 
-	// Auto-submit on radio button selection
-	$(".glossCat input[type='radio']").on("change", function () {
-		submitForm();
-		$(this).parent().addClass('active').siblings().removeClass('active');
-	});
-	$('.glossBtn').click(function(){
-		$(this).parents('.glossItems').toggleClass('cntToggle').siblings().removeClass('cntToggle');
-	})
+  // Auto-submit on radio button selection
+  $(".glossCat input[type='radio']").on("change", function () {
+    submitForm();
+    $(this).parent().addClass("active").siblings().removeClass("active");
+  });
+  $(".glossBtn").on("click", function () {
+    $(this)
+      .parents(".glossItems")
+      .toggleClass("cntToggle")
+      .siblings()
+      .removeClass("cntToggle");
+  });
 });
