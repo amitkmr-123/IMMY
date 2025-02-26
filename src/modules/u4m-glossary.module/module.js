2f8a7e3c7a0b8b0b8126e39a9fe71b30088e9dfa
pinkiwebplant <pinki@thewebplant.in>
2025-02-26 17:57:48 +0530

    Split Form Module Done

--- src/modules/u4m-glossary.module/module.js
+++ src/modules/u4m-glossary.module/module.js
@@ -1,39 +1,48 @@
 $(document).ready(function () {
-  function submitForm() {
-    let formData = $(".glossTop").serialize(); // Get form data
+	function submitForm() {
+		let formData = $(".glossTop").serialize(); // Get form data
 
-    $.ajax({
-      url: window.location.pathname, // Keep current URL
-      type: "GET",
-      data: formData,
-      beforeSend: function () {
-        $(".glossList").html("<p>Loading...</p>");
-      },
-      success: function (response) {
-        let newHtml = $(response).find(".glossList").html();
-        $(".glossList").html(newHtml);
-      },
-      error: function () {
-        $(".glossList").html("<p>Error loading results.</p>");
-      },
-    });
-  }
+		$.ajax({
+			url: window.location.pathname, // Keep current URL
+			type: "GET",
+			data: formData,
+			beforeSend: function () {
+				$(".glossList").html("<div class='errMsg'><div class='page-center'><p>Loading...</p></div></div>");
+			},
+			success: function (response) {
+				let newHtml = $(response).find(".glossList").html();
+				$(".glossList").html(newHtml);
+			},
+			error: function () {
+				$(".glossList").html("<div class='errMsg'><div class='page-center'><p>Error loading results.</p></div></div>");
+			},
+		});
+	}
+	function ToggleClass() {
+		$(".glossBtn").on("click", function () {
+			$(this)
+				.parents(".glossItems")
+				.toggleClass("cntToggle")
+				.siblings()
+				.removeClass("cntToggle");
+		});
+	}
+	// Auto-submit on search input change
+	$(".glossSearch input[type='search']").on("input", function () {
+		submitForm();
+		setTimeout(function() {
+			ToggleClass();
+		}, 1500);
+	});
 
-  // Auto-submit on search input change
-  $(".glossSearch input[type='search']").on("input", function () {
-    submitForm();
-  });
+	// Auto-submit on radio button selection
+	$(".glossCat input[type='radio']").on("change", function () {
+		submitForm();
+		setTimeout(function() {
+			ToggleClass();
+		}, 1500);
+		$(this).parent().addClass("active").siblings().removeClass("active");
+	});
 
-  // Auto-submit on radio button selection
-  $(".glossCat input[type='radio']").on("change", function () {
-    submitForm();
-    $(this).parent().addClass("active").siblings().removeClass("active");
-  });
-  $(".glossBtn").on("click", function () {
-    $(this)
-      .parents(".glossItems")
-      .toggleClass("cntToggle")
-      .siblings()
-      .removeClass("cntToggle");
-  });
+	ToggleClass();
 });
